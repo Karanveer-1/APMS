a01e5d9948c42190730ebb4287ea1776023f15ed
Amy Hong <amyhong0502@gmail.com>
2019-03-26 04:01:46 -0700

    extracted BASE_URL from URL strings

--- apms-tester/test/specs/login.success.spec.js
+++ apms-tester/test/specs/login.success.spec.js
@@ -1,14 +1,16 @@
 const assert = require('assert');
 
+const BASE_URL = 'http://localhost:8080/APMS';
+
 describe('APMS page', () => {
   it('should have the right title', () => {
-    browser.url('http://phobos.edu.bcit.ca:8080/APMS');
+    browser.url(BASE_URL);
     const title = browser.getTitle();
     assert.equal(title, 'Login');
   });
 
   it('should be able to login with valid credentials', () => {
-    browser.url('http://phobos.edu.bcit.ca:8080/APMS');
+    browser.url(BASE_URL);
 
     const username = $('#loginGrid\\:usernameInput');
     username.setValue('admin');
