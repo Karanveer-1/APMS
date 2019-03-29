20ea7c33f6726c48368c87b2d0c918cea67b3723
mikah13 <anhminhhoang13@gmail.com>
2019-03-29 08:31:24 -0700

    update

--- JavaSource/controller/ProjectController.java
+++ JavaSource/controller/ProjectController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 
 import javax.faces.context.FacesContext;
@@ -20,7 +21,7 @@ import model.Employee;
 import model.Project;
 
 @Named("projectController")
-@ConversationScoped
+@SessionScoped
 public class ProjectController implements Serializable {
 
 	@Inject
